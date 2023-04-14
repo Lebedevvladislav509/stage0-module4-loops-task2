9d5d0b43c32bb9d77d575afdc168a406578ef8a0
LVLAD2009@yandex.ru <24233lebedko>
2023-04-14 17:22:07 +0300

    stage0-module4-loops-task2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int a =0;
+        int fuk = 0;
+        while(a<=printToInclusive){
+            fuk *=fuk;
+            System.out.println(fuk);
+            a++;
+            fuk++;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int a = 0;
+        while (a < 2) {
+            a++;
+        }
+        int b = 2;
+        while (a < printToInclusive) {
+            while (b % a == 0 && b <= a) {
+                System.out.println(a);
+                b++;
+            }
+            a++;
+        }
     }
 }
