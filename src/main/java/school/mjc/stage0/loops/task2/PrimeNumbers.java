4b4ebc499208dc952a148046a16545c6536d785a
LVLAD2009@yandex.ru <24233lebedko>
2023-04-17 14:24:06 +0300

    stage0-module4-loops-task2

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -3,15 +3,6 @@ package school.mjc.stage0.loops.task2;
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
 
-        out:
-        for (int i = 2; i <= printToInclusive; i++) {
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    continue out;
-                }
-            }
-            System.out.println(i);
-        }
     }
 }
 
